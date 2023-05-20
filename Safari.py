851a86e22f3ec488d92430104661bf02fa7288b2
FerumB3 <saha81869@gmail.com>
2023-05-20 11:55:18 +0300

    first.commit

--- Safari.py
+++ Safari.py
@@ -31,3 +31,53 @@ class Student:
         elif self.progress > 5:
             print("Passed e[ternally...")
             self.alive = False
+
+    def end_of_day(self):
+        print(f'Gladness = {self.gladness}')
+        print(f'Progress = {round(self.progress,2)}')
+
+    def live(self,day):
+         day = "Day" + str(day) + "of" + self.name + "life"
+         print (f'{day:=^50})')
+         print('1 - for study')
+         print('2 - for sleep')
+         print('3 - for chill')
+         live_cube = (input('Enter your choice:'))
+         if  live_cube == '1':
+            print('You entered 1 variant (study)')
+            self.to_study()
+
+         elif live_cube == "2":
+            print('You entered 2 variant (sleep)')
+            self.to_sleep()
+
+         elif  live_cube == "3":
+            print('You entered 3 variant (chill)')
+            self.to_chill()
+         else:
+            print("! Not correct choice")
+         self.end_of_day()
+         self.is_alive()
+
+         if live_cube == 2 < 20:
+            print("You need some rest")
+
+         if live_cube == 1 < 20:
+            print("You need some study")
+
+         if live_cube == 2 > 20:
+            print("You need some rest")
+
+         if live_cube == 2 > 20:
+             print("You need some chill ")
+
+
+
+nick=Student(name='Nick')
+for day in range(365):
+    if nick.alive==False:
+       print("Student Lose")
+       break
+    nick.live(day)
+
+
