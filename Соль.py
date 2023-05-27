ddc63c22026f940783a327cfaebc5d198a2e7457
FerumB3 <saha81869@gmail.com>
2023-05-27 10:18:23 +0300

    first.commit

---
+++ Соль.py
@@ -0,0 +1,99 @@
+#class Humen:
+    #height = 170
+    #satiety = 50
+#class Student(Humen):
+    #satiety=0
+
+#class Worker(Humen):
+   # setiety=0
+
+#nick=Student()
+#ann=Worker()
+#print(nick.height)
+#print(nick.height)
+#print(ann.height)
+#print(ann.satiety)
+
+# class Grandperend:
+#     height=170
+#     satiety = 100
+#     age=60
+#
+# class Perent(Grandperend):
+#     age = 40
+#
+# class Child(Perent):
+#     height = 50
+# def__init__(self):
+#      print(self.height)
+
+# class Wow:
+#     def __wow(self):
+#         print('Wow')
+#     def _hello(self):
+#         print("hello")
+#
+# some_obj=Wow()
+# some_obj.hello()
+# some_obj._Wow_wow()
+
+# class Hello_word:
+#     hello="Hello"
+#     _hello_="Hello"
+#     __hello__="__Hello__"
+#     def __init__(self):
+#         self.world="World"
+#         self._world_="_world_"
+#         self.__world__ = "__world__"
+
+#     def preinter(self):
+#         print(self.Hello)
+#         print(self._Hello)
+#         print(self.__Hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+#
+# class H1(Hello_word):
+#     def h1_print(self):
+#         print(self.Hello)
+#         print(self._Hello)
+#         print(self.__Hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+
+# class Hello:
+#     def __init__(self):
+#         print("Hello")
+# class Hello_World(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print("World!")
+#
+# obj=Hello_World()
+
+# class Class1:
+#     var=20
+#     def __init__(self):
+#         self.var=10
+#
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super(Class2,self).__init__()
+#         print(self.var)
+#         print(super().var)
+# hello_world=Class2
+
+# class Computer:
+#     def caculute(self):
+#         print("Calculiting")
+#
+# class Display
+#     display(self):
+#     print("Dispalalalalal")
+#
+# class Smarphone(Display,Computer)
+#     pass
+
